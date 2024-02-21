49c948e2b49dcb3e1e8e94aa4c00008437bcc11e
KatrineG <eaa23kgs@students.eaaa.dk>
2024-02-21 09:11:36 +0100

    Kopi af filer

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# B-rneBasketFonden
+# Responsiv CSS og introduktion til JavaScript
+
+Her er filerne til Værktøjskasse 3.
+
+## Hej Verden 
---
+++ js/js.js
@@ -0,0 +1,50 @@
+/**
+ * file: js/js.js
+ * purpose: Behaviors
+ **/
+console.log('Der er kontakt til js/js.js')
+
+let a = "Hej Julemand" 
+let b = 23 // tal
+let c = false
+let julemandenFindes = true
+
+const and = "Pekingand"
+
+
+ /** kontrolstrukturer*/ 
+
+
+console.log( b * b)
+console.log( "Findes Julemanden virkelig? " +julemandenFindes)
+ if( c === false)
+ console.log ("ja")
+
+
+
+ /**InnerHTML: Sådan*/
+
+ document.getElementById("title").innerHTML = "Mojn do"
+
+ /* JavaScript Introduktion
+ */
+
+// let a = "Hej"
+// console.log(a)
+
+/** Øvelse: Toggle Class 
+ * 
+ * Lav en knap, der viser eller skuler en menu
+ * 
+*/
+/*
+// HTML:
+// tilføj denne knap: <button onclick="viseSkjule()">Skjule eller vise</button>
+
+// JavaScript:
+function viseSkjule() {
+    var element = document.getElementById("smaller");
+    element.classList.toggle("skjuler");
+    // flere toggles mv. kan tilføjes her
+ }
+ */
